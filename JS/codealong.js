c65b883da114ed8c48ea56b0733d30e4c3deb61b
AGD3 <49073762+AGD3@users.noreply.github.com>
2020-04-04 05:58:58 -0700

    initial push, 'characters'

---
+++ JS/codealong.js
@@ -0,0 +1,67 @@
+// // // how to create an object literal
+// // const book = {
+// //     title: "Harry Potter",
+// //     author: "JK Rowling",
+// //     nuPages: 250,
+// //     genre: ["science fiction", "fiction"],
+// //     hasRead: true
+// // }
+
+// // // how to create an object constructor
+// // function song(name, isPopular) {
+// //     this.name = "Happy Birthday";
+// //     this.isPopular = true;
+// // }
+
+// // // access book object using dot notation
+// // console.log(book.author);
+
+
+// // // access
+// // console.log(song["name"]);
+// // console.log(song["isPopular"]);
+
+
+// // Students use the 4 images to create 2 objects and 2 object constructors that will display the properties on the console log. Requirements will be 2 strings, 1 boolean, 1 integer. Selena, Garnet, Black Panther, Ada Wong
+
+// const selena = {
+//     primary: "singer",
+//     occupation: ["songwriter", "actress"],
+//     isPopular: true,
+//     method: function song(hot, dance) {
+//         console.log( );
+//     }
+// };
+
+// console.log(selena.primary);
+
+
+// const kingtChalla = {
+//     primary: "king",
+//     occupation: ["actor", "nobleman"],
+//     isPopular: true
+// };
+
+// function Garnet(primary, occupation, isPopular) {
+//     this.primary = primary;
+//     this.occupation = occupation;
+//     this.isPopular = isPopular;
+//     this.method = function method(cool, animation) {
+//         console.log(...console.log.apply.)
+//     };
+// };
+
+// let cartoon = new Garnet ("lion", "tiger", "bear");
+
+// let animation = new Garnet ("scifi", "fiction");
+
+// console.log(animation);
+
+let celebretes = ["Cool", "Rich", Popular"];
+// let i;
+let text = "";
+for (i = 0; i < celebretes.length; i++) {
+    text += ` ${celebretes[i]}\n `
+}
+
+console.log(text); 
